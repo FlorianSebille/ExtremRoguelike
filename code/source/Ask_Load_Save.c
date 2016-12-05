7e781a20a57a475d02684d3fe037488df63af340
Xenos72 <lardy.anthony@hotmail.com>
2016-12-05 22:42:41 +0100

    Page d'accueil (done soon)

--- code/source/Ask_Load_Save.c
+++ code/source/Ask_Load_Save.c
@@ -3,8 +3,10 @@
 int Ask_Load_Save(){
 	int startxSave, startySave, widthSave, heightSave;
 	int startxStart, startyStart, widthStart, heightStart;
+	int startxRules, startyRules, widthRules, heightRules;
 	WINDOW *Save_Win;
 	WINDOW *Start_Win;
+	WINDOW *Rules_Win;
 	cbreak();
 	keypad(stdscr,TRUE);
 	timeout(0);
@@ -17,30 +19,36 @@ int Ask_Load_Save(){
 	widthStart = 170;
 	startxStart = 2;
 	startyStart = 2;
+	heightRules = 22;
+	widthRules = 30;
+	startxRules = 10;
+	startyRules = 10;
 	Start_Win=create_newwin(heightStart,widthStart,startyStart,startxStart,"Roguelike");
-	mvwprintw(Start_Win,10,110,"					     ,-.");
-	mvwprintw(Start_Win,10,110,"			___,---.__          /'|`\          __,---,___");
-	mvwprintw(Start_Win,10,110,"	             ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.");
-	mvwprintw(Start_Win,10,110,"		   ,'        |           ~'\     /`~           |        `.");
-	mvwprintw(Start_Win,10,110,"		  /      ___//              `. ,'          ,  , \___      \");
-	mvwprintw(Start_Win,10,110,"             |    ,-'   `-.__   _         |        ,    __,-'   `-.    |");
-	mvwprintw(Start_Win,10,110,"		 |   /          /\_  `   .    |    ,      _/\          \   |");
-	mvwprintw(Start_Win,10,110,"		 \  |           \ \`-.___ \   |   / ___,-'/ /           |  /");
-	mvwprintw(Start_Win,10,110,"		  \  \           | `._   `\\  |  //'   _,' |           /  /");
-	mvwprintw(Start_Win,10,110,"		   `-.\         /'  _ `---'' , . ``---' _  `\         /,-'");
-	mvwprintw(Start_Win,10,110,"		      ``       /     \    ,='/ \`=.    /     \       ''");
-	mvwprintw(Start_Win,10,110,"			      |__   /|\_,--.,-.--,--._/|\   __|");
-	mvwprintw(Start_Win,10,110,"			      /  `./  \\`\ |  |  | /,//' \,'  \");
-	mvwprintw(Start_Win,10,110,"			     /   /     ||--+--|--+-/-|     \   \");
-	mvwprintw(Start_Win,10,110,"			    |   |     /'\_\_\ | /_/_/`\     |   |");
-	mvwprintw(Start_Win,10,110,"			     \   \__, \_     `~'     _/ .__/   /");
-	mvwprintw(Start_Win,10,110,"			      `-._,-'   `-._______,-'   `-._,-'*/");
-		
+	mvwprintw(Start_Win,10,138,",-.");
+	mvwprintw(Start_Win,11,118,"___,---.__         /'|`\\          __,---,___" );
+	mvwprintw(Start_Win,12,115,",-'    \\`    `-.____,-' |  `-.____,-'    //    `-." );
+	mvwprintw(Start_Win,13,112,",'        |           ~'\\     /`~           |        `." );
+	mvwprintw(Start_Win,14,111,"/      ___//              `. ,'          ,  , \\___      \\" );
+	mvwprintw(Start_Win,15,110,"|    ,-'   `-.__   _         |        ,    __,-'   `-.    |");
+	mvwprintw(Start_Win,16,110,"|   /          /\\_  `   .    |    ,      _/\\          \\   |");
+	mvwprintw(Start_Win,17,110,"\\  |           \\ \\`-.___ \\   |   / ___,-'/ /           |  /");
+	mvwprintw(Start_Win,18,111,"\\  \\           | `._    `\\  |  /'    _,'  |           /  /");
+	mvwprintw(Start_Win,19,112,"`-.\\         /'  _ `---'' , . ``---' _   `\\         /,-'");
+	mvwprintw(Start_Win,20,115,"``       /     \\    ,='/ \\`=.    /      \\       ''");
+	mvwprintw(Start_Win,21,123,"|__   /|\\_,--.,-.--,--._/|\\   __|");
+	mvwprintw(Start_Win,22,123,"/  `./  \\\\`\\ |  |  | /,//' \\,'  \\");
+	mvwprintw(Start_Win,23,122,"/   /     ||--+--|--+-/-|    \\    \\");
+	mvwprintw(Start_Win,24,121,"|   |     /'\\_\\_\\ | /_/_/`\\    |   |");
+	mvwprintw(Start_Win,25,121,"\\   \\__,  \\_     `~'     _/ .__/   /");
+	mvwprintw(Start_Win,26,122,"`-._,-'    `-._______,-'   `-._,-'");		
+	Rules_Win=create_newwin(heightRules,widthRules,startyRules,startxRules,"Rules");
+
 	Save_Win=create_newwin(heightSave,widthSave,startySave,startxSave,"Sauvegarde");
 	mvwprintw(Save_Win,2,2,"Voulez vous charger une partie sauvegardée ?");
 	mvwprintw(Save_Win,6,widthSave*0.05,"LEFTWARDS ARROW: Oui");
 	mvwprintw(Save_Win,6,widthSave*0.60,"RIGHTWARDS ARROW: Non");
 	wrefresh(Save_Win);
+	wrefresh(Rules_Win);
 	wrefresh(Start_Win);
 	do{
 		choix = getch();
@@ -51,26 +59,3 @@ int Ask_Load_Save(){
 	delwin(Save_Win);
 	return choix;
 }
-
-/*
-
-
-					     ,-.
-			___,---.__          /'|`\          __,---,___
-<<<<<<< HEAD
-	             ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.
-		   ,'        |           ~'\     /`~           |        `.
-		  /      ___//              `. ,'          ,  , \___      \
-                 |    ,-'   `-.__   _         |        ,    __,-'   `-.    |
-		 |   /          /\_  `   .    |    ,      _/\          \   |
-		 \  |           \ \`-.___ \   |   / ___,-'/ /           |  /
-		  \  \           | `._   `\\  |  //'   _,' |           /  /
-		   `-.\         /'  _ `---'' , . ``---' _  `\         /,-'
-		      ``       /     \    ,='/ \`=.    /     \       ''
-			      |__   /|\_,--.,-.--,--._/|\   __|
-			      /  `./  \\`\ |  |  | /,//' \,'  \
-			     /   /     ||--+--|--+-/-|     \   \
-			    |   |     /'\_\_\ | /_/_/`\     |   |
-			     \   \__, \_     `~'     _/ .__/   /
-			      `-._,-'   `-._______,-'   `-._,-'*/
-
